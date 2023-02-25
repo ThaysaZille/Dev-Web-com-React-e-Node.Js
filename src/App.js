c39a12a359385742f68c582629c7ad30b34a829e
Thaysa Zille <61854138+ThaysaZille@users.noreply.github.com>
2023-02-24 22:08:55 -0300

    Aula03

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
-import Noticia from "./components/noticia/noticia";
-function App() {
-  const [noticias, setNoticias]= React.useState([]);
+import React from 'react';
+import './App.css'
 
+let i = 1;
+
+function App() {
+  const [pokemon, setPokemon] = React.useState({});
+  function proximo(){
+    i = i+1;
+    Carregar()
+  }
+  function anterior(){
+    i = i-1;
+    Carregar()
+  }
   function Carregar(){
-    setNoticias([{ 
-        imagem: "https://picsum.photos/200/300",
-        titulo: "TItulo 1",
-        texto: "Mussum Ipsum, cacilds vidis litro abertis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.Quem num gosta di mé, boa gentis num é.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
-      },
-      {
-        imagem: "https://picsum.photos/200/300",
-        titulo: "TItulo 2",
-        texto: "2Mussum Ipsum, cacilds vidis litro abertis. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.Quem num gosta di mé, boa gentis num é.Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi.Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."
-      },
-    ]);
+    fetch('https://pokeapi.co/api/v2/pokemon/' + i)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setPokemon(data);
+    });
   }
 
+  return (
+    
+    pokemon.sprites ?
+      <div className='App'>
 
-  return noticias.length >= 2 ?
-    (<div className="container">
-      
-      <button onClick={Carregar}>Carregar Noticias</button>
-      <Noticia imagem={noticias[0].imagem} titulo={noticias[0].titulo} texto={noticias[0].texto}/>
-      <Noticia imagem={noticias[1].imagem} titulo={noticias[1].titulo} texto={noticias[1].texto}/>
-    </div>
-    ):(
-    <div className="container"> 
-      <button onClick={Carregar}>Carregar Noticias</button>
-      <div>Sem noticias</div>
-    </div>
-    )
+        <button onClick={anterior}>Back</button> <button onClick={proximo}>Next</button>
+        <img src={pokemon.sprites.front_default}/>
+        <label>Nome:{pokemon.name} Habilidade:{pokemon.abilities[0].ability.name}</label>
 
-
-  ;
+      </div>
+    :
+      <div className='Inicio'>
+        <button onClick={Carregar}>Carregar Pokemons</button>
+      </div>
+  );
 }
 
 export default App;
